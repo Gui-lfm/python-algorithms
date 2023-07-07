0f580e2d0550316d757ab862f2c8799c2e3e4254
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-07-07 18:37:29 -0300

    requisito 03 in progress

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word is None:
+        return False
+
+    if len(word) <= 1:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(
+            word[1:-1], low_index + 1, high_index - 1
+        )
+
+    return False
+
+
+print(is_palindrome_recursive("", 0, 2))
