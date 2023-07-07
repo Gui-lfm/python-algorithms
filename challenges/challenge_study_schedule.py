671afac8a3a7aa0004e67ca1b5ef0d6cebd878dd
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-07-07 15:24:30 -0300

    req 1 in progress

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period: list[tuple], target_time):
+    if not isinstance(target_time, int):
+        return None
+
+    total_students = 0
+    for tupla in permanence_period:
+        if not all(isinstance(element, int) for element in tupla):
+            return None
+        if target_time in tupla:
+            total_students += 1
+
+    return total_students
+
+# print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 2))
