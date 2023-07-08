028e8e304d2e913a3824065de0208172fe0d101c
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-07-07 22:53:17 -0300

    anagrams in progress

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,10 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+def is_anagram(first_string: str, second_string: str):
+    first_string = first_string.lower()
+    second_string = second_string.lower()
 
-    raise NotImplementedError
+    if len(first_string) != len(second_string):
+        return False
+    if first_string == "" or second_string == "":
+        return False
+
+    return ("", "", True)
