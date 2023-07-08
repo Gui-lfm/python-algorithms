0de57e629f386c5d6198c37c1f0490b4ad834efa
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-07-08 00:44:06 -0300

    palindrome recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,16 +1,13 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    if word is None:
+def is_palindrome_recursive(word: str, low_index: int, high_index: int):
+    if word == "":
         return False
 
-    if len(word) <= 1:
+    if low_index >= high_index:
         return True
 
     if word[low_index] == word[high_index]:
-        return is_palindrome_recursive(
-            word[1:-1], low_index + 1, high_index - 1
-        )
+        low_index = low_index + 1
+        high_index = high_index - 1
+        return is_palindrome_recursive(word, low_index, high_index)
 
     return False
-
-
-print(is_palindrome_recursive("", 0, 2))
